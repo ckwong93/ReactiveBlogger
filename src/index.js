3d765de921baedc9ebb645e488b3f523a4f0cddf
Calvin Kwong <ckwong93@gmail.com>
2017-05-30 21:45:11 -0700

    add router switch to fix reactrouter bug

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import ReduxPromise from 'redux-promise';
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
@@ -15,10 +15,12 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     {/* <App /> */}
     <BrowserRouter>
-      <div>
-        <Route path="/" component={PostsIndex}/>
-        <Route path="/posts/new" component={PostsNew} />
-      </div>
+      <Switch>
+        <div>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/" component={PostsIndex}/>
+        </div>
+      </Switch>
     </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
